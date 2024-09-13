7a6aabbc11361e0a5298a39525ebea59be3b03c2
Angelina Ivanova <linaivanova509@gmail.com>
2024-09-13 22:39:53 +0400

    Separation into components

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,17 @@
 import Header from "./components/Header"
-import WayToTeach from "./components/WayToTeach"
-import Button from "./components/Button/Button";
-import { useState } from "react";
-import { ways, differences } from "./data"
+import TeachingSection from "./components/Button/TeachingSection";
+import DifferencesSection from "./components/Button/DifferencesSection";
+import IntroSection from "./components/Button/introSection";
+
 
 function App() {
-  const [ contentType, setContentType] = useState(null) 
-
-  function handleClick(type) {
-    setContentType(type)
-  }
-
   return (
     <div>
       <Header />
       <main>
-        <section>
-          <h3>Наш подход к обучению</h3>
-
-          <ul>
-            <WayToTeach 
-            title={ways[0].title} 
-            description={ways[0].description} 
-            />
-             <WayToTeach {...ways[1]} />
-             <WayToTeach {...ways[2]} />
-             <WayToTeach {...ways[3]} />          
-          </ul>
-        </section>
-        <section>
-          <h3>Чем мы отличаемся от других</h3>
-
-          <Button onClick={() => handleClick("way")}>Подход</Button>
-          <Button onClick={() => handleClick("easy")}>Доступность</Button>
-          <Button onClick={() => handleClick("program")}>Концентрация</Button>
-
-          { !contentType && <p>Нажми на кнопку</p>}
-          { contentType && <p>{differences[contentType]}</p> }
-
-        </section>
+          <IntroSection />
+          <TeachingSection />
+          <DifferencesSection />
       </main>
     </div>  
   )
--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -11,4 +11,4 @@ export default function Button({ children, onClick, isActive }) {
     )
 
 }
-
+ 
